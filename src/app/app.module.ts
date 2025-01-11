820fff7f94478489a23ec562122d7a45cc05a278
pacograndez <paco9194_gc@hotmail.com>
2025-01-10 22:32:08 -0500

    Crear componente Breadcrumbs, Header, Sidebar y la estructura para el layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BreadcrumbsComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
